568a75f431056d8b2988f6c0b9ac016be3a38135
esteng <elias.stengel@gmail.com>
2023-04-18 15:20:01 -0400

    scripts to preprocess data

--- src/semantic_parsing_with_constrained_lm/scripts/process_all_lamp.sh
+++ src/semantic_parsing_with_constrained_lm/scripts/process_all_lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for split in 0-100 10-90 20-80 30-70 40-60 60-40 70-30 80-20 90-10 100-0; do
+for split in 0-100 10-90 20-80 30-70 40-60 50-50 60-40 70-30 80-20 90-10 100-0; do
     for dtype in lisp fol; do
         in_dir="/brtx/602-nvme1/estengel/ambiguous_parsing/data/raw/${split}-5k-train-100-perc-ambig_${dtype}"
         out_dir="/brtx/602-nvme1/estengel/ambiguous_parsing/data/processed/${split}-5k-train-100-perc-ambig_${dtype}_fewshot"
---
+++ src/semantic_parsing_with_constrained_lm/scripts/process_zero_shot_lamp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##for split in pp_fol scope_fol revscope_fol bound_fol conj_fol; do
+for split in bound_fol; do
+    in_dir="/brtx/602-nvme1/estengel/ambiguous_parsing/data/raw/generalization/${split}"
+    out_dir="/brtx/602-nvme1/estengel/ambiguous_parsing/data/processed/${split}"
+    python scripts/process_lamp.py --in_dir $in_dir --out_dir $out_dir
+done
